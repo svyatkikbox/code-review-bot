bf0fbea942933f1bc9c7e55e16a26ab3ddc89b59
Irish Man <swt@MacBook-Pro-Irish.local>
2021-05-12 23:18:34 +0300

    feat(commands) wrap all commands in one export

---
+++ src/commands/all.ts
@@ -0,0 +1,14 @@
+import { help, helpHandler } from './help';
+import { menu, menuHandler } from './menu';
+import { registration } from './registration';
+import { showNeedReview, showNeedReviewHandler } from './showNeedReview';
+
+export {
+	help,
+	helpHandler,
+	menu,
+	menuHandler,
+	registration,
+	showNeedReview,
+	showNeedReviewHandler,
+};
