4a5723014096674ef869458151408751f75e305f
Irish Man <swt@MacBook-Pro-Irish.local>
2021-05-12 23:10:37 +0300

    feat(scenes) implement step for project finding through bot

---
+++ src/scenes/registration/steps/find-project-step.ts
@@ -0,0 +1,30 @@
+import { Scenes } from 'telegraf';
+
+import { ProjectRepo } from '../../../app';
+
+const stepId = 'FindProjects';
+const findProjects = new Scenes.BaseScene<Scenes.SceneContext>(stepId);
+
+findProjects.enter(ctx => ctx.reply('Enter project name'));
+findProjects.leave(ctx => ctx.reply('Bye'));
+findProjects.command('exit', ctx => ctx.scene.leave());
+findProjects.on('text', async ctx => {
+	const projectName = ctx.message.text;
+	let projectData = null;
+
+	try {
+		projectData = await ProjectRepo.getProjectByName(projectName);
+		console.log(projectData);
+	} catch (error) {
+		return ctx.reply('Cannot find project');
+	}
+
+	if (!projectData) {
+		return ctx.reply('Cannot find project');
+	}
+
+	await ctx.reply('Project was found');
+	return ctx.scene.leave();
+});
+
+export const findProjectsStep = { stepId, step: findProjects };
