863744fad77bcb4ce55d82a43fbf2f320aa3a0e9
Irish Man <swt@MacBook-Pro-Irish.local>
2021-05-13 18:35:32 +0300

    feat: add shutdowm handlers

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,20 @@ import { bot } from './app';
 bot.launch();
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
+
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
+
+process.once('beforeExit', () => console.log('about to exit '));
+
+process.on('exit', () => bot.stop('exit'));
+
 process.on('unhandledRejection', err => {
-	console.error(err);
-	bot.stop('SIGTERM');
+	console.error('unhandledRejection', { err });
+	bot.stop('unhandledRejection');
+	process.exit(1);
+});
+
+process.on('uncaughtException', err => {
+	console.log(`Uncaught Exception: ${err.message}`);
+	process.exit(1);
 });
