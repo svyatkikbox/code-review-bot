d760c93391f5e5033e64b2a3d875441a3901d981
Irish Man <swt@MacBook-Pro-Irish.local>
2021-05-14 14:25:19 +0300

    refactor: showNeedReviewCommand implemented trough class with constructor injection

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,9 @@ import { GitlabAPI } from './gitlab/gitlab-api';
 import { httpService } from './gitlab/http-service';
 import { ProjectRepository } from './gitlab/repositories/projects/repository';
 import { UserRepository } from './gitlab/repositories/users/repository';
+import { SubscriptionRepository } from './subscription/repository';
 import { registrationScene } from './scenes/registration/registration-scene';
+import { ShowNeedReview } from './commands/show-need-review';
 
 const { BOT_TOKEN, PORT, WEBHOOK_URL } = config;
 const bot = new Telegraf<Scenes.SceneContext>(BOT_TOKEN);
@@ -22,11 +24,13 @@ const tg = new Telegram(BOT_TOKEN);
 
 bot.telegram.setWebhook(`${WEBHOOK_URL}/bot${BOT_TOKEN}:${PORT}`);
 
-tg.setMyCommands([help, menu, registration, showNeedReview]);
-
 const gitlabAPI = new GitlabAPI(httpService);
 const UserRepo = new UserRepository(gitlabAPI);
 const ProjectRepo = new ProjectRepository(gitlabAPI);
+const SubscriptionRepo = new SubscriptionRepository();
+const showNeedReviewCommand = new ShowNeedReview(ProjectRepo, SubscriptionRepo);
+
+tg.setMyCommands([help, menu, registration, showNeedReviewCommand.botCommand]);
 
 const stage = new Scenes.Stage<Scenes.SceneContext>([
 	...registrationScene.steps,
@@ -39,7 +43,9 @@ bot.command(registration.command, ctx =>
 	ctx.scene.enter(registrationScene.enteringStepId)
 );
 bot.command(menu.command, ctx => menuHandler(ctx));
-bot.command(showNeedReview.command, ctx => showNeedReviewHandler(ctx));
-bot.hears('Куда позвали на ревью', ctx => showNeedReviewHandler(ctx));
+bot.command(showNeedReviewCommand.botCommand.command, ctx =>
+	showNeedReviewCommand.handler(ctx)
+);
+bot.hears('Куда позвали на ревью', ctx => showNeedReviewCommand.handler(ctx));
 
-export { bot, UserRepo, ProjectRepo };
+export { bot, UserRepo, ProjectRepo, SubscriptionRepo };
