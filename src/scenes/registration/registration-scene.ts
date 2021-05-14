5b0bb2ce91ebd2952a815d133761f6f2be9d52fc
Irish Man <swt@MacBook-Pro-Irish.local>
2021-05-14 14:50:50 +0300

    feat: infer type in registration scene

--- src/scenes/registration/registration-scene.ts
+++ src/scenes/registration/registration-scene.ts
@@ -1,7 +1,8 @@
+import { BotScene } from '../types';
 import { findProjectsStep } from './steps/find-project-step';
 import { findUserStep } from './steps/find-user-step';
 
-export const registrationScene = {
+export const registrationScene: BotScene = {
 	enteringStepId: findUserStep.stepId,
 	steps: [findUserStep.step, findProjectsStep.step],
 };
