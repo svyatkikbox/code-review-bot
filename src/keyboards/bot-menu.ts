9d7c6b0f1dcfacf9e3607033250f27c1682148a2
Irish Man <swt@MacBook-Pro-Irish.local>
2021-06-07 23:37:01 +0300

    feat: created folder for bot's keyboards

---
+++ src/keyboards/bot-menu.ts
@@ -0,0 +1,12 @@
+import { KeyboardButton } from 'telegraf/typings/core/types/typegram';
+
+import { dictionary } from '../dictionary';
+
+const showNeedReviewsBtn: KeyboardButton = {
+	text: dictionary.buttons.showNeedReviews,
+};
+const showMyOpenMrsCommandBtn: KeyboardButton = {
+	text: dictionary.buttons.showMyOpenMrsCommand,
+};
+
+export const menuKeyboard = [showNeedReviewsBtn, showMyOpenMrsCommandBtn];
