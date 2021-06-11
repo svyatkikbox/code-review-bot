9c0708f4055c2efd0ff639d2bb47ad3883c9bf0f
Irish Man <swt@MacBook-Pro-Irish.local>
2021-06-11 22:53:18 +0300

    refactor: move condition calculation and estimate date extraction in separete methods

--- src/gitlab/repositories/projects/repository.ts
+++ src/gitlab/repositories/projects/repository.ts
@@ -1,9 +1,12 @@
-import compareAsc from 'date-fns/compareAsc';
+import compareDesc from 'date-fns/compareDesc';
+import isAfter from 'date-fns/isAfter';
+
 import { GitlabAPI } from '../../gitlab-api';
 import {
 	Mention,
 	MergeRequest,
 	MergeRequestNote,
+	MergeRequestReviewAwards,
 	Project,
 	User,
 } from '../types';
@@ -59,53 +62,96 @@ export class ProjectRepository implements IProjectRepository {
 		return userMentions;
 	}
 
+	getLastUserEstimateDate(
+		userName: string,
+		mergeRequestAwards: MergeRequestReviewAwards
+	): Date | null {
+		const userLike = mergeRequestAwards.likes.find(
+			award => award.userName === userName
+		);
+		const userDislike = mergeRequestAwards.dislikes.find(
+			award => award.userName === userName
+		);
+
+		if (!userLike && !userDislike) return null;
+
+		const lastUserEstimateDate = [userLike, userDislike]
+			.map(estimate => estimate?.createdAt)
+			.filter(estimate => !!estimate)
+			.map(createdAt => new Date(createdAt as string))
+			.sort(compareDesc)[0];
+
+		return lastUserEstimateDate;
+	}
+
+	/*
+	 * звать если
+	 * - есть дискуссии !resolvable
+	 * - - если лайк не стоит
+	 * - - если отметка стоит после того, как поставил лайк
+	 * - есть дискуссии resolvable && !resolved с пользователем?
+	 */
+	shouldUserBeCalledInMergeRequest(
+		userName: string,
+		userMentions: Mention[],
+		mergeRequestAwards: MergeRequestReviewAwards
+	): boolean {
+		let shoulBeCalled = false;
+
+		const unResolvedMentions = userMentions.filter(
+			mention => mention.resolvable && !mention.resolved
+		);
+		const unResolvableMentions = userMentions.filter(
+			mention => !mention.resolvable
+		);
+		const lastUserEstimateDate = this.getLastUserEstimateDate(
+			userName,
+			mergeRequestAwards
+		);
+
+		const isEstimated = !!lastUserEstimateDate;
+		const isThereAnyUnresolvedMention = !!unResolvedMentions.length;
+		const isThereAnyUnresolvableMention = !!unResolvableMentions.length;
+		const lastUnresolvableMentionDate = isThereAnyUnresolvableMention
+			? new Date(unResolvableMentions[0].createdAt)
+			: null;
+		const isMentionedAfterEstimated =
+			!!lastUnresolvableMentionDate &&
+			isAfter(
+				new Date(lastUnresolvableMentionDate),
+				lastUserEstimateDate || new Date(0)
+			);
+
+		if (
+			(isThereAnyUnresolvableMention && !isEstimated) ||
+			(isThereAnyUnresolvableMention && isMentionedAfterEstimated) ||
+			isThereAnyUnresolvedMention
+		) {
+			shoulBeCalled = true;
+		}
+
+		return shoulBeCalled;
+	}
+
 	async getProjectReviewCalls(projectId: number): Promise<any[]> {
 		const mergeRequestsData = await this.gitlabAPI.getProjectMergeRequestsData(
 			projectId
 		);
 
-		const mrs: Mention[] = [];
+		const mrs: MergeRequest[] = [];
 		for (const mrData of mergeRequestsData) {
 			const [mrNotes, mrAwards] = await Promise.all([
 				this.gitlabAPI.getMergeRequestNotes(projectId, mrData.id),
 				this.gitlabAPI.getMergeRequestAwards(projectId, mrData.id),
 			]);
 
-			/*
-			 * 1 получили инфу про мр полную
-			 * 2 получили по его id дискуссии и лукасы/дизлукасы
-			 * 3 нужно понять, есть ли юзер в дискуссах
-			 * 4 позвали ли пользователя в этом МР?
-			 * - есть ли дискуссии resolvable && !resolved с пользователем?
-			 * - есть ли дискуссии !resolvable с пользователем после его лайка/дизлайка (поздней самой оценки)?
-			 */
-
 			const userName = 'svyat'; // TODO получаем из подписки
 			const userMentions = this.extractMentionsFromNotes(userName, mrNotes);
-			const notResolvedNotes = userMentions.filter(
-				mention => mention.resolvable && !mention.resolved
-			);
-			const notResolvableNotes = userMentions.filter(
-				mention => !mention.resolvable
-			);
-			const userLike = mrAwards.likes.find(
-				award => award.userName === userName
-			);
-			const userDislike = mrAwards.dislikes.find(
-				award => award.userName === userName
-			);
-			const lastUserEstimate = [userLike, userDislike]
-				.map(estimate => estimate?.createdAt)
-				.filter(estimate => !!estimate)
-				.map(createdAt => new Date(createdAt as string))
-				.sort(compareAsc);
 
-			const lastNotResolvableNoteMention = notResolvableNotes
-				.map(mention => new Date(mention.createdAt))
-				.sort(compareAsc);
-
-			if (!lastUserEstimate.length && notResolvableNotes.length) {
-				mrs.push(notResolvableNotes[0]);
+			if (
+				this.shouldUserBeCalledInMergeRequest(userName, userMentions, mrAwards)
+			) {
+				mrs.push(mrData);
 			}
 		}
 
