0f87833d32154388f6f4d1c8e32b9f0d6f54d28e
Irish Man <swt@MacBook-Pro-Irish.local>
2021-07-28 23:39:21 +0300

    feat: rename subscription table in subscriptions and create mergeRequests table. in will be used to cache project mrs' data related

--- schema.sql
+++ schema.sql
@@ -6,9 +6,19 @@ CREATE TABLE IF NOT EXISTS Users
 	chat_id integer NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS Subscription
+CREATE TABLE IF NOT EXISTS Subscriptions
 (
 	id SERIAL PRIMARY KEY,
 	user_id integer NOT NULL,
 	projects integer[] NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS MergeRequests
+(
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(255),
+	upvotes integer NOT NULL,
+	downvotes integer NOT NULL,
+	web_url string,
+	labels VARCHAR(255)[]
 );
