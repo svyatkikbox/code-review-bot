d31865c3b8033557c94058edeecdc4c521565da5
Irish Man <swt@MacBook-Pro-Irish.local>
2021-07-28 23:17:35 +0300

    feat: implement http service for wrapping axios and not to spread 3rd party interfaces in app. move paginated search from gitlab api in it. and renamed in fetchPaginated

--- src/gitlab/http-service.ts
+++ src/gitlab/http-service.ts
@@ -1,3 +1,57 @@
-import axios, { AxiosStatic } from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 
-export const httpService: AxiosStatic = axios;
+import { PaginationParams } from './types';
+
+interface IRequestConfig extends AxiosRequestConfig {
+	baseURL?: string;
+	headers?: Record<string, string>;
+	params?: any;
+	data?: any;
+}
+export interface IHttpService {
+	get<T>(url: string, requestConfig?: IRequestConfig): Promise<T>;
+
+	fetchPaginated<T>(url: string, params?: PaginationParams): Promise<T[]>;
+}
+export class HttpService implements IHttpService {
+	http: AxiosInstance;
+
+	constructor(config: IRequestConfig) {
+		this.http = axios.create(config);
+	}
+
+	async get<T>(url: string, requestConfig?: IRequestConfig): Promise<T> {
+		const { data } = await this.http.get(url, requestConfig);
+
+		return data;
+	}
+
+	async fetchPaginated<T>(
+		url: string,
+		params?: PaginationParams
+	): Promise<T[]> {
+		const perPage = params?.perPage || 100;
+		let page = params?.page || 1;
+
+		const data = [];
+		let end = false;
+
+		while (end !== true) {
+			const response = await this.http.get(url, {
+				params: {
+					per_page: perPage,
+					page,
+				},
+			});
+
+			data.push(...response.data);
+			page++;
+
+			if (!response.data.length) {
+				end = true;
+			}
+		}
+
+		return data as T[]; // TODO add runtime validation
+	}
+}
